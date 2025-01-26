df9bc830c89a2159b3b6b4f51115a3a18c420e20
batuhanckmkc <>
2025-01-26 22:23:21 +0300

    Updated scriptable objects

--- Assets/Scripts/View/RewardItem/WheelRewardItem.cs
+++ Assets/Scripts/View/RewardItem/WheelRewardItem.cs
@@ -1,8 +1,15 @@
 
+using GameDeveloperDemo.Model;
+using UnityEngine;
+
 namespace GameDeveloperDemo.View
 {
     public class WheelRewardItem : RewardItem
     {
-
+        public override void SetRewardUI(Sprite sprite)
+        {
+            rewardImage.sprite = sprite;
+            rewardText.text = ZoneRewardData.rewardConfigurationData.rewardType == RewardType.Bomb ? "Bomb" : $"x{ZoneRewardData.amount}";
+        }
     }
 }
